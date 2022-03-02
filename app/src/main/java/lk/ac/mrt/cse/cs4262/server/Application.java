f597758007c3c5f983c99d46de872a7d53369f3b
Lakshan Weerasinghe <lak98weerasinghe@gmail.com>
2022-03-03 03:05:59 +0530

    application.properties file added

--- app/src/main/java/lk/ac/mrt/cse/cs4262/server/Application.java
+++ app/src/main/java/lk/ac/mrt/cse/cs4262/server/Application.java
@@ -1,5 +1,10 @@
 package lk.ac.mrt.cse.cs4262.server;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 import lk.ac.mrt.cse.cs4262.server.chatRoom.MainHall;
 import lk.ac.mrt.cse.cs4262.server.client.command.NewIdentityHandler;
 
@@ -7,7 +12,15 @@ public class Application {
 
     public static void main(String[] args){
 
-        Server server = new Server(6000);
+        Properties props = new Properties();
+        try (InputStream ins = new FileInputStream("src/main/resources/application.properties")) {
+            props.load(ins);
+            ins.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Server server = new Server(Integer.parseInt(props.getProperty("port")));
         server.setStore(Store.getInstance());
         server.setMainHall(MainHall.getInstance("MainHall-s1"));
         server.setNewIdentityHandler(new NewIdentityHandler(Store.getInstance(),
