3ebefe8862a3bcc4c4be9903d399801cb8e053a9
Lakshan Weerasinghe <lak98weerasinghe@gmail.com>
2022-03-03 11:23:18 +0530

    logger added

--- app/src/main/java/lk/ac/mrt/cse/cs4262/server/Application.java
+++ app/src/main/java/lk/ac/mrt/cse/cs4262/server/Application.java
@@ -5,12 +5,20 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import lk.ac.mrt.cse.cs4262.server.chatRoom.MainHall;
 import lk.ac.mrt.cse.cs4262.server.client.command.NewIdentityHandler;
 
 public class Application {
 
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+
     public static void main(String[] args){
+
+        log.info("application is starting");
+        log.debug("application debug");
 
         Properties props = new Properties();
         try (InputStream ins = new FileInputStream("src/main/resources/application.properties")) {
